92e3dc464064f8a96a48197069d24347440590b2
Kurogi <30377909+Kurogi1314@users.noreply.github.com>
2017-08-07 05:57:56 +0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-client.login('MzQzODI3Njc5ODI2NTQyNTky.DGkaeQ.Z4I9Z8IYleJAANWYT_geZPzv9dY');
