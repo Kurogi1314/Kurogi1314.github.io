2d8168f3458c25874f0753a4b633f69d49cc80dc
Kurogi <30377909+Kurogi1314@users.noreply.github.com>
2017-08-08 01:32:53 +0700

    - add bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+  const client = new Discord.Client();
+  
+  client.login('MzQzODI3Njc5ODI2NTQyNTky.DGpDkw.BEA7ox_8nCyY5MxkF402vg3tq3g');
+client.on("ready", () => {
+  console.log("I am ready!");
+});
