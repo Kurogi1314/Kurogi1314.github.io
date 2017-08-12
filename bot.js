3317aef8e9b8adf69ca3bf2e9bd45111a1dc8587
Kurogi <30377909+Kurogi1314@users.noreply.github.com>
2017-08-12 14:14:46 +0700

    Delete bot.js

--- bot.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js");
-  const client = new Discord.Client();
-  
-  client.login('MzQzODI3Njc5ODI2NTQyNTky.DGpDkw.BEA7ox_8nCyY5MxkF402vg3tq3g');
-client.on("ready", () => {
-  console.log("I am ready!");
-});
